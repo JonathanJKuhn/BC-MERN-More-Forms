76ac34e23bee1b159886fab64407f96033f99496
Jonathan Kuhn <jkuhn96@gmail.com>
2022-10-04 14:53:18 -0400

    Added basic for validation per instructions

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,24 +11,64 @@ const Form = () => {
         <div>
             <form>
                 <div>
-                    <label htmlFor="fname">First Name:</label>
+                    <label htmlFor="fname">First Name: </label>
                     <input type="text" name="fname" onChange={(e) => setFname(e.target.value)} />
+                    {
+                        !fname
+                        ? ''
+                        : fname.length < 2
+                            ? <p>First Name must be at least 2 characters</p>
+                            : ''
+                            
+                    }
                 </div>
                 <div>
-                    <label htmlFor="lname">Last Name:</label>
+                    <label htmlFor="lname">Last Name: </label>
                     <input type="text" name="lname" onChange={(e) => setLname(e.target.value)} />
+                    {
+                        !lname
+                        ? ''
+                        : lname.length < 2
+                            ? <p>Last Name must be at least 2 characters</p>
+                            : ''
+                            
+                    }
                 </div>
                 <div>
-                    <label htmlFor="email">Email:</label>
+                    <label htmlFor="email">Email: </label>
                     <input type="text" name="email" onChange={(e) => setEmail(e.target.value)} />
+                    {
+                        !email
+                        ? ''
+                        : email.length < 2
+                            ? <p>Email must be at least 2 characters</p>
+                            : ''
+                            
+                    }
                 </div>
                 <div>
-                    <label htmlFor="pass">Password:</label>
+                    <label htmlFor="pass">Password: </label>
                     <input type="password" name="pass" onChange={(e) => setPass(e.target.value)} />
+                    {
+                        !pass
+                        ? ''
+                        : pass.length < 2
+                            ? <p>Password must be at least 8 characters</p>
+                            : ''
+                            
+                    }
                 </div>
                 <div>
-                    <label htmlFor="conPass">Confirm Password:</label>
+                    <label htmlFor="conPass">Confirm Password: </label>
                     <input type="password" name="conPass" onChange={(e) => setConPass(e.target.value)} />
+                    {
+                        !conPass
+                        ? ''
+                        : conPass !== pass
+                            ? <p>Passwords must match</p>
+                            : ''
+                            
+                    }
                 </div>
             </form>
             <div>
